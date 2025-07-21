600a6960917fdec350a636ac55a5371cc8d661bd
kui2331 <157229009+kui2331@users.noreply.github.com>
2025-07-21 08:53:09 +0800

    初始化

---
+++ config/routes.ts
@@ -0,0 +1,17 @@
+﻿export default [
+  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { path: '/', redirect: '/add_chart' },
+  { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
+  { path: '/my_charts', name: '我的图表', icon: 'lineChart', component: './MyCharts' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', name: '管理页面', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '管理页面2', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
