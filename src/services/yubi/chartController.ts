2d67f427a40ee3ae64c911dc4464201afef11d2a
kui2331 <157229009+kui2331@users.noreply.github.com>
2025-07-25 17:50:35 +0800

    武器：feat(ui): 添加异步生成图表功能

--- src/services/yubi/chartController.ts
+++ src/services/yubi/chartController.ts
@@ -48,6 +48,41 @@ export async function editChartUsingPOST(
 }
 
 /** genChartByAi POST /api/chart/gen */
+export async function genChartByAiAsyncUsingPOST(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.genChartByAiUsingPOSTParams,
+  body: {},
+  file?: File,
+  options?: { [key: string]: any },
+) {
+  const formData = new FormData();
+
+  if (file) {
+    formData.append('file', file);
+  }
+
+  Object.keys(body).forEach((ele) => {
+    const item = (body as any)[ele];
+
+    if (item !== undefined && item !== null) {
+      formData.append(
+        ele,
+        typeof item === 'object' && !(item instanceof File) ? JSON.stringify(item) : item,
+      );
+    }
+  });
+
+  return request<API.BaseResponseBiResponse_>('/api/chart/gen/async', {
+    method: 'POST',
+    params: {
+      ...params,
+    },
+    data: formData,
+    requestType: 'form',
+    ...(options || {}),
+  });
+}
+/** genChartByAi POST /api/chart/gen/async */
 export async function genChartByAiUsingPOST(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.genChartByAiUsingPOSTParams,
